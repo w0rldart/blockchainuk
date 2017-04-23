a94401a3deb3bd411078fea55f36929664f2956a
Alex Budurovici <w0rldart@w0rldart.com>
2017-04-23 10:35:00 +0100

    Ensure a window is created on macOS when win is null

--- main.js
+++ main.js
@@ -34,3 +34,11 @@ app.on('window-all-closed', () => {
         app.quit();
     }
 });
+
+app.on('activate', () => {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (win === null) {
+        createWindow()
+    }
+});
