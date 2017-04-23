66f6090fda3f966eb0a9e0b0d4134fb49377891c
Alex Budurovici <w0rldart@w0rldart.com>
2017-04-23 10:31:55 +0100

    Create a window when app is ready

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
-const {app, BrowserWindow} = require('electron');
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const url = require('url');
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let win;
+
+function createWindow() {
+    win = new BrowserWindow({ width: 800, heigth: 600 });
+
+    win.loadUrl(url.format({
+        pathname: path.join(__dirname, 'index.html');
+        protocol: 'file:',
+        slashes: true
+    }));
+
+    win. webContents.openDevTools();
+
+    win.on('closed', () => {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this it the time
+        // when you should dlete the corresponding element.
+        win = null;
+    })
+}
+
+app.on('ready', createWindow)
